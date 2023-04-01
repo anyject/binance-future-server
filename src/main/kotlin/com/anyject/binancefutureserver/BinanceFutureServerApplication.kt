4fe84001b5e29ebf0789f9e3ca187c79cf24c2aa
malleus35 <malleus35@gmail.com>
2023-04-01 21:16:58 +0900

    gitflow-feature-stash: TEST-1

--- src/main/kotlin/com/anyject/binancefutureserver/BinanceFutureServerApplication.kt
+++ src/main/kotlin/com/anyject/binancefutureserver/BinanceFutureServerApplication.kt
@@ -7,5 +7,6 @@ import org.springframework.boot.runApplication
 class BinanceFutureServerApplication
 
 fun main(args: Array<String>) {
+    println("HELLO")
     runApplication<BinanceFutureServerApplication>(*args)
 }
