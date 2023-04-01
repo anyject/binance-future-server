2ff8f211e6e5fc5400d0502cf41f07ed4f55e4c5
malleus35 <malleus35@gmail.com>
2023-04-01 19:53:30 +0900

    NO-ISSUE Init Project

---
+++ src/main/kotlin/com/anyject/binancefutureserver/BinanceFutureServerApplication.kt
@@ -0,0 +1,11 @@
+package com.anyject.binancefutureserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BinanceFutureServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<BinanceFutureServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/anyject/binancefutureserver/BinanceFutureServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anyject.binancefutureserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BinanceFutureServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
