b5f175da7d17d7766216c5f1ab545bdf3ac35387
malleus35 <malleus35@gmail.com>
2023-04-07 01:05:18 +0900

    BIN-2 Add OpenFeignClientTest to send queryParameters

--- src/test/kotlin/com/anyject/binancefutureserver/config/OpenFeignClientTest.kt
+++ src/test/kotlin/com/anyject/binancefutureserver/config/OpenFeignClientTest.kt
@@ -1,12 +1,14 @@
 package com.anyject.binancefutureserver.config
 
 import feign.Feign
+import feign.QueryMap
 import feign.RequestLine
 import feign.Response
 import feign.jackson.JacksonDecoder
 import feign.jackson.JacksonEncoder
 import feign.okhttp.OkHttpClient
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.junit.jupiter.MockitoExtension
@@ -16,18 +18,36 @@ import org.springframework.http.HttpStatus
 
 @ExtendWith(MockitoExtension::class)
 class OpenFeignClientTest {
+    private lateinit var client: BinanceFutureFeignClient
+
+
+    @BeforeEach
+    fun init() {
+        client = OpenFeignConfig().getClient();
+    }
+
 
     @Test
     fun `OpenFeignClient Basic Connection Test`() {
-        val client = OpenFeignConfig().getClient()
         assertThat(client.checkConnectionToServer().status()).isEqualTo(HttpStatus.OK.value())
     }
-}
 
+    @Test
+    fun `Send klines request and retrieve response body`() {
+        val queryMap = HashMap<String, Any>()
+        queryMap["symbol"] = "BTCUSDT"
+        queryMap["interval"] = "5m"
+        queryMap["limit"] = 750
+        val response = client.retrieveKline(queryMap)
+        assertThat(response.status()).isEqualTo(HttpStatus.OK.value())
+        assertThat(response.body()).isNotNull
+    }
+    
+}
 @EnableFeignClients
 class OpenFeignConfig {
     val apiUrl = "https://testnet.binancefuture.com/fapi/v1"
-    fun getClient() : BinanceFutureFeignClient {
+    fun getClient(): BinanceFutureFeignClient {
         return Feign.builder()
             .client(OkHttpClient())
             .encoder(JacksonEncoder())
@@ -35,8 +55,14 @@ class OpenFeignConfig {
             .target(BinanceFutureFeignClient::class.java, apiUrl)
     }
 }
+
 @FeignClient(name = "BinanceFutureFeignClient", url = "https://testnet.binancefuture.com/fapi/v1")
 interface BinanceFutureFeignClient {
     @RequestLine("GET /ping")
     fun checkConnectionToServer(): Response
+
+    @RequestLine("GET /klines")
+    fun retrieveKline(
+       @QueryMap queryMap: Map<String, Any>
+    ): Response
 }
