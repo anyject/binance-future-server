827540077ff7aec10b18c02d4fe5fee32b2e36da
malleus35 <malleus35@gmail.com>
2023-04-14 04:32:11 +0900

    BIN-6 Modify Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.anyject"
-version = "0.0.1-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 val springCloudVersion = "2022.0.2"
 
