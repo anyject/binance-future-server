ff62483a701eda0418b54f85e325477031d8e922
malleus35 <malleus35@gmail.com>
2023-04-15 18:37:16 +0900

    BIN-6 컴파일 오류 수정

--- src/test/kotlin/com/anyject/binancefutureserver/config/ConfigurationPropertiesTest.kt
+++ src/test/kotlin/com/anyject/binancefutureserver/config/ConfigurationPropertiesTest.kt
@@ -18,13 +18,13 @@ class ConfigurationPropertiesTest {
 
     @Test
     fun `ApplicationProperties 최상단 프로퍼티 불러오기 테스트`() {
-        assertThat(applicationProperties.apiKey).isNotEmpty
-        assertThat(applicationProperties.apiKey).isInstanceOf(String::class.java)
+        assertThat(applicationProperties.binance.apiKey).isNotEmpty
+        assertThat(applicationProperties.binance.apiKey).isInstanceOf(String::class.java)
     }
 
     @Test
     fun `ApplicationProperties 객체 프로퍼티 불러오기 테스트`() {
-        assertThat(applicationProperties.defaultUrls).isNotNull
-        assertThat(applicationProperties.defaultUrls).isInstanceOf(ApplicationProperties.DefaultUrls::class.java)
+        assertThat(applicationProperties.binance.defaultUrls).isNotNull
+        assertThat(applicationProperties.binance.defaultUrls).isInstanceOf(ApplicationProperties.Binance.DefaultUrls::class.java)
     }
 }
